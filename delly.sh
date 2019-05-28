fcfb8f7f3e5d2785fe5e856dfb20a6a6515f8ddd
Tobias Meissner <meissner.t@googlemail.com>
2019-05-28 11:01:20 -0500

    Update delly.sh

--- delly.sh
+++ delly.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
+set -x
+source $MODULESHOME/init/bash
 
-module load delly
+module load bcftools
 
-#exclusions=human.hg19.excl.tsv
-outdir=/data/storage/14MS10038/delly
-reference=/data/database/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa
-tumor=/data/s3/averapatients/14MS10038/wgs-tumor/pat001t-ready.bam
-germline=/data/s3/averapatients/14MS10038/wgs-blood/pat001n-ready.bam
+delly=/home/tobias/bin/delly_v0.8.1_linux_x86_64bit
+hg19excl=/home/tobias/bin/human.hg19.excl.tsv
+out=/data/storage/ikmb_gastric/delly
+ref=/data/database/Homo_sapiens/b37decoy/hs37d5.fa
 
-mkdir -p  $outdir
+tbam=$1
+gbam=$2
+name=$(basename "$tbam" .bam)
 
-delly -t DEL -o $out_dir/del.vcf -g $reference $tumor $germline
+tid=$(basename "$tbam" _realigned_sorted.bam)
+gid=$(basename "$gbam" _realigned_sorted.bam)
+
+awk -v tid="$tid" -v gid="$gid" 'BEGIN {print tid "\t" "tumor" "\n" gid "\t" "control"}' > $out/$tid.txt
+
+$delly call \
+	-x $hg19excl \
+	-o $out/$name.bcf \
+	-g $ref \
+	-q 20 \
+	-s 15 \
+	$1 $2
+
+$delly filter \
+	-f somatic \
+	-o $out/$name.somatic.bcf \
+	-p \
+	-s $out/$tid.txt \
+	$out/$name.bcf
+
+
+bcftools view $out/$name.somatic.bcf > $out/$name.somatic.vcf
 
 exit 0
